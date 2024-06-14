242d4493aed9a5ffc67e978dae34a0ccd7cb5bf5
jordanoboyle <jmcmcode@gmail.com>
2024-06-13 23:50:13 -0600

    bikeDelete dynamic from Details modal

--- src/BikeDelete.jsx
+++ src/BikeDelete.jsx
@@ -5,9 +5,10 @@ export function BikeDelete(props) {
 
   const submitDelete = () => {
     console.log("submitting delete");
-    axios.delete(`http://localhost:3000/bikes/11.json`).then((response) => {
-      console.log("Deleted Bike Confirm", response.data)
+    axios.delete(`http://localhost:3000/bikes/${props.bike.id}.json`).then((response) => {
+      console.log("Deleted Bike Confirm", response.data);
     })
+    window.location.href = "/"
   }
 
   return(
